63762845e5963973d778cfb9a50a2d208b80fa6a
Arthur Mattiollo Redin <aredin@furb.br>
2026-03-16 20:08:49 -0300

    AR: Adicionado diagrama

--- Trabalho1/obj/Debug/net8.0/Trabalho1.AssemblyInfo.cs
+++ Trabalho1/obj/Debug/net8.0/Trabalho1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trabalho1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d0975216d00855978a943a131b243a61daf3a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trabalho1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trabalho1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
